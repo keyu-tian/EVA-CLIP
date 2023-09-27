8544a072415ef4db43c9a955c2289658f95f3c21
keyu-tian <k_code@163.com>
2023-09-28 04:20:07 +0800

    [init] remove other folders

--- EVA-02/asuka/eva_clip/__init__.py
+++
@@ -1,5 +0,0 @@
-from .clip import *
-from .eva_clip import *
-from .model import *
-from .simple_tokenizer import *
-from .vit_model import *
--- EVA-02/seg/backbone/__init__.py
+++
@@ -1,4 +0,0 @@
-from .eva2 import EVA2
-
-
-__all__ = ['EVA2']
